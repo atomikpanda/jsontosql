d874880cc5eb16aa0a57028075abde3720feea88
Bailey Seymour (atomikpanda) <b.seymour@me.com>
2018-02-26 19:02:26 -0500

    Initial Commit

---
+++ jsontosql/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  jsontosql
+//
+//  Created by Bailey Seymour on 2/26/18.
+//  Copyright © 2018 Bailey Seymour. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
